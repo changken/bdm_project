268079e8e4e7fb1692c682f6cdc1a0abf3ae5064
changken <pp24513@gmail.com>
2021-12-23 10:19:56 +0800

    train attack_classification model && feature_selection

--- notebooks/attack_classification/src/model.py
+++ notebooks/attack_classification/src/model.py
@@ -3,16 +3,37 @@ import torch.nn as nn
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
+#         self.linear_relu_stack = nn.Sequential(
+#             nn.Linear(196, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, 10),
+#         )
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(196, 512),
+            nn.Linear(59, 4096),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.BatchNorm1d(4096),
+            nn.Dropout(0.25),
+            nn.Linear(4096, 8192),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.BatchNorm1d(8192),
+            nn.Dropout(0.25),
+            nn.Linear(8192, 16384),
+            nn.ReLU(),
+            nn.BatchNorm1d(16384),
+            nn.Dropout(0.25),
+#             nn.Linear(16384, 8192),
+#             nn.ReLU(),
+#             nn.BatchNorm1d(8192),
+#             nn.Dropout(0.25),
+#             nn.Linear(8192, 4096),
+#             nn.ReLU(),
+#             nn.BatchNorm1d(4096),
+#             nn.Dropout(0.25),
+            nn.Linear(16384, 10)
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
